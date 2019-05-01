442631d442791ff29930bcd322d38492486dab30
Ramakrishna <crkreddy91@gmail.com>
2019-05-01 10:36:41 +0530

    second project

---
+++ Selenuimproject/src/com/hdfc/car/carloans/FirstClass.java
@@ -0,0 +1,11 @@
+package com.hdfc.car.carloans;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hai to all");
+System.out.println("goodmorning");
+	}
+
+}
